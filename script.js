44df58b75b3333120a4248f67830a7f9482d7040
LukasJohns <lukas.johns@ocsbstudent.ca>
2021-02-18 19:32:25 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let bacon = 0
+document.getElementById('press').addEventListener('click',myInput)
+function myInput() {
+  bacon = document.getElementById('start').value
+  bacon = parseInt(bacon)
+  if (bacon < 0) {
+    alert('the interger is negative')
+  } else {
+    alert('the interger is positive')
+  }
+}
