2e5b3d2045be181ef454754655adbb67c8ca4bae
LukasJohns <lukas.johns@ocsbstudent.ca>
2021-02-18 19:53:52 +0000

    Linting round 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let bacon = 0
-document.getElementById('press').addEventListener('click',myInput)
-function myInput() {
+document.getElementById('press').addEventListener('click', myInput)
+function myInput () {
   bacon = document.getElementById('start').value
   bacon = parseInt(bacon)
   if (bacon < 0) {
